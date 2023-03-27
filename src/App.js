4da151672999636bc0a6653a06cd5395916e3df1
MidnightYKT <nomiama_ykt@mail.ru>
2023-03-27 13:36:51 -0700

    Added API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import "./scss/app.scss";
+import React, { useState, useEffect } from 'react'
+import './scss/app.scss'
 
-import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import Header from './components/Header'
+import Categories from './components/Categories'
+import Sort from './components/Sort'
+import PizzaBlock from './components/PizzaBlock'
 
 function App() {
-  return (
-    <div class="wrapper">
-      <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-            <PizzaBlock />
-          </div>
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        fetch('https://6421ee0c86992901b2bf51ae.mockapi.io/items')
+            .then((res) => res.json())
+            .then((arr) => {
+                setItems(arr)
+            })
+    }, [])
+
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
+                        <Sort />
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {items.map((obj) => (
+                            <PizzaBlock key={obj.id} {...obj} />
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
