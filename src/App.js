d9feffcfb4931f618263072d66774f0fbb0fff2c
MidnightYKT <nomiama_ykt@mail.ru>
2023-03-27 16:03:24 -0700

    Added Skeleton and API(mocko)

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import './scss/app.scss'
 import Header from './components/Header'
 import Categories from './components/Categories'
 import Sort from './components/Sort'
-import PizzaBlock from './components/PizzaBlock'
+import SneakerBlock from './components/SneakerBlock'
+import Skeleton from './components/SneakerBlock/Skeleton'
 
 function App() {
     const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         fetch('https://6421ee0c86992901b2bf51ae.mockapi.io/items')
             .then((res) => res.json())
             .then((arr) => {
                 setItems(arr)
+                setIsLoading(false)
             })
     }, [])
 
@@ -28,9 +31,9 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {items.map((obj) => (
-                            <PizzaBlock key={obj.id} {...obj} />
-                        ))}
+                        {isLoading
+                            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+                            : items.map((obj) => <SneakerBlock key={obj.id} {...obj} />)}
                     </div>
                 </div>
             </div>
