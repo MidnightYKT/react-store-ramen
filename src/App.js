e5abd494596353e1703924fe78f0986b052d90b9
MidnightYKT <nomiama_ykt@mail.ru>
2023-03-27 20:14:54 -0700

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import './scss/app.scss'
+import { Route, Routes } from 'react-router-dom'
 
 import Header from './components/Header'
-import Categories from './components/Categories'
-import Sort from './components/Sort'
-import SneakerBlock from './components/SneakerBlock'
-import Skeleton from './components/SneakerBlock/Skeleton'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import NotFound from './pages/NotFound'
 
 function App() {
-    const [items, setItems] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-
-    useEffect(() => {
-        fetch('https://6421ee0c86992901b2bf51ae.mockapi.io/items')
-            .then((res) => res.json())
-            .then((arr) => {
-                setItems(arr)
-                setIsLoading(false)
-            })
-    }, [])
-
     return (
         <div className="wrapper">
             <Header />
             <div className="content">
                 <div className="container">
-                    <div className="content__top">
-                        <Categories />
-                        <Sort />
-                    </div>
-                    <h2 className="content__title">Все пиццы</h2>
-                    <div className="content__items">
-                        {isLoading
-                            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-                            : items.map((obj) => <SneakerBlock key={obj.id} {...obj} />)}
-                    </div>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
                 </div>
             </div>
         </div>
