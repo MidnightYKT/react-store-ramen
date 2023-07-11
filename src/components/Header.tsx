73def9676dbd689005cef09413b2deee8f76e312
MidnightYKT <nomiama_ykt@mail.ru>
2023-07-11 15:16:08 -0700

    update data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,10 +14,10 @@ function Header() {
             <div className="container">
                 <Link to="/">
                     <div className="header__logo">
-                        <img width="38" src="img/pizza-logo.svg" alt="Pizza logo" />
+                        <img width="65" src="img/ramen-logo.png" alt="Pizza logo" />
                         <div>
-                            <h1>React Pizza</h1>
-                            <p>самая вкусная пицца во вселенной</p>
+                            <h1>React Ramen</h1>
+                            <p>the most delicious rolls in the universe</p>
                         </div>
                     </div>
                 </Link>
